bcb9d68c93dd69035d0fa21b5630b9fce90cbc94
Andreas Wilcox <andreas.wilcox@m-sys.se>
2019-03-04 21:12:14 +0100

    Sets visits to 0 for stats if no results are available

--- backend/src/stats.ts
+++ backend/src/stats.ts
@@ -1,7 +1,13 @@
 export class Stats {
-    lastHour!: number;
-    lastDay!: number;
-    lastWeek!: number;
+    lastHour: number;
+    lastDay: number;
+    lastWeek: number;
+
+    constructor() {
+        this.lastHour = 0;
+        this.lastDay = 0;
+        this.lastWeek = 0;
+    }
 }
 
 export type StatsDictionary = { [page: string]: Stats};
