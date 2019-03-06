1250517093840bb1a43a293c193d4c142432775f
Andreas Wilcox <andreas.wilcox@m-sys.se>
2019-03-06 13:52:33 +0100

    Prints what page was logged

--- web/src/logVisit.js
+++ web/src/logVisit.js
@@ -1,15 +1,16 @@
+var page = document.currentScript.getAttribute('page');
+
 var xhr = new XMLHttpRequest();
 xhr.open("POST", "http://pagestatistics.anwilc.com/visit", true);
 xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 xhr.onreadystatechange = () => {
     if(xhr.readyState === 4)
-        console.log("Logged visit")
+        console.log("Logged visit with " + page);
 }
 xhr.onerror = () => {
-    console.log("Unable to log visit to statistics service")
+    console.log("Unable to log visit to statistics service");
 }
 
-var page = document.currentScript.getAttribute('page');
 xhr.send(JSON.stringify({
     "page": page
 }));
